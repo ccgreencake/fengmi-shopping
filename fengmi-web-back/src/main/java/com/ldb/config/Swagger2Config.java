c3f0cb5593557f8d0fe04eec19650f14dbd52c9e
Liu Dongbin <dobi0108@icloud.com>
2023-11-08 15:30:53 +0800

    增加了springboot的热部署； 配置了最基本的swagger2文档

---
+++ fengmi-web-back/src/main/java/com/ldb/config/Swagger2Config.java
@@ -0,0 +1,47 @@
+package com.ldb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @Author LiuDongBin
+ * @Date 2023/11/08 15:07
+ **/
+@Configuration
+@EnableSwagger2
+public class Swagger2Config {
+    @Bean
+    public Docket getDocket(){
+        //创建Docket对象
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())//指定API接口文件首页信息
+                .select()//初始化并返回一个API选择构造器
+                .apis(RequestHandlerSelectors.basePackage("com.ldb.controller"))//为任何接口生成API文档
+                .paths(PathSelectors.any())//可以根据url路径设置哪些请求加入文档，忽略哪些请求
+                .build(); //创建API文档
+        return docket;
+    }
+
+
+    private ApiInfo getApiInfo(){
+        //创建作者 对象
+        Contact contact = new Contact("刘董彬","http://localhost:8080/products","dobi0108@icloud.com");
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("锋迷商城API文档") //文档标题
+                .description("文档的描述信息...")//文档描述
+                .contact(contact)//文档作者
+                .version("V1.0")//文档版本
+                .build(); //构建
+        return apiInfo;
+    }
+}
