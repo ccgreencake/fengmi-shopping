3c1ce631efcfdae517f580a4a0bcf06d53251787
Liu Dongbin <dobi0108@icloud.com>
2023-11-13 11:21:47 +0800

    修改了MyWebConfig,增加了外部静态资源路径

--- fengmi-web-portal/src/main/java/com/ldb/config/MyWebConfig.java
+++ fengmi-web-portal/src/main/java/com/ldb/config/MyWebConfig.java
@@ -2,6 +2,7 @@ package com.ldb.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -12,6 +13,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  **/
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file:/Users/dobi/Desktop/img/");
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
