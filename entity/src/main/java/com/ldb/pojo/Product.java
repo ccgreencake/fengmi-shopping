1a75d597bfc016e77297279b5ace3a73c20ac0f2
Liu Dongbin <dobi0108@icloud.com>
2023-11-13 19:44:09 +0800

    推荐页面新增商品的时间数据

--- entity/src/main/java/com/ldb/pojo/Product.java
+++ entity/src/main/java/com/ldb/pojo/Product.java
@@ -66,13 +66,13 @@ public class Product implements Serializable {
     /**
      * 创建时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     private Date createTime;
 
     /**
      * 更新时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    @JsonFormat(pattern = "yyyy-MM-dd",timezone = "GMT+8")
     private Date updateTime;
 
 
